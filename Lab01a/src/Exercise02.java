337a1a28478506c46a3500874615b76f88265fc0
darrancebeh <darrancebeh@gmail.com>
2024-04-30 09:34:55 +0800

    EDIT: Made output look like solution output

--- Lab01a/src/Exercise02.java
+++ Lab01a/src/Exercise02.java
@@ -7,7 +7,7 @@ public class Exercise02 {
             int n = scanner.nextInt();
 
             String nString = Integer.toString(n);
-
+            System.out.printf("The digits in %d are: ", n);
             for(int i = 0; i < 5; i++) {
                 System.out.printf("%c ", nString.charAt(i));
             }
