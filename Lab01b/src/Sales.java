dbf2c365d66d2412db505fd5e2f70e0dcde82b95
darrancebeh <darrancebeh@gmail.com>
2024-05-07 08:22:28 +0800

    EDIT: Edit Lab01b to be identical to lab sample output

--- Lab01b/src/Sales.java
+++ Lab01b/src/Sales.java
@@ -7,31 +7,32 @@ public class Sales {
             double totalProduct2 = 0;
             double totalProduct3 = 0;
             while(true) {
-                System.out.println("Enter product number (1-3) (0 to stop): ");
+                System.out.print("Enter product number (1-3) (0 to stop): ");
                 int product = scanner.nextInt();
                 
                 if (product == 0) {
                     break;
                 }
 
-                System.out.println("Enter quantity sold: ");
+                System.out.print("Enter quantity sold: ");
                 int quantity = scanner.nextInt();
 
-                 if(product == 1) {
-                    totalProduct1 += quantity * 2.98;
-                 }
-                 else if (product == 2) {
-                    totalProduct2 += quantity * 4.50;
-                 }
-                 else if (product == 3) {
-                    totalProduct3 += quantity * 9.98;
-                 }
-                 else {
-                    System.out.println("Invalid product number.");
-                 }
-
+                switch(product) {
+                    case 1:
+                        totalProduct1 += quantity * 2.98;
+                        break;
+                    case 2:
+                        totalProduct2 += quantity * 4.50;
+                        break;
+                    case 3:
+                        totalProduct3 += quantity * 9.98;
+                        break;
+                    default:
+                        System.out.println("Invalid product number.");
+                        break;
+                }
             }
-            System.out.printf("Product 1: $%.2f%nProduct 2: $%.2f%nProduct 3: $%.2f", totalProduct1, totalProduct2, totalProduct3);
+            System.out.printf("\nProduct 1: $%.2f%nProduct 2: $%.2f%nProduct 3: $%.2f", totalProduct1, totalProduct2, totalProduct3);
         }
     }
 }
